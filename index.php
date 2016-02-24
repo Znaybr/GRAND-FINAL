688b8eeb66e665eeb6495f031b446ab8428f7126
tverjus <thibaut.verjus@gmail.com>
2016-02-24 11:45:11 -0500

    Ajout du fichier de départ index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    // initialisation ici
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>$Title$</title>
+</head>
+<body>
+
+<p>Bonjour Chris </p>
+
+</body>
+</html>
