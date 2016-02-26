6fa03eb59da8211938bfc3c97af95ba5f0cfd4ef
ctack <christack50@gmail.com>
2016-02-26 14:11:28 -0500

    dossier defines

---
+++ _defines.php
@@ -0,0 +1,6 @@
+<?php
+define('SITE_NAME','bijouxelise');
+define('SITE_URL','http:/bijouxelise.projetisi.com');
+define('PAGE_ID', 'page_id');
+define('USER_ISLOGGED', 'user_is_logged');//L'utilisateur est connecté
+define('MAIN_MENU','main_menu');
