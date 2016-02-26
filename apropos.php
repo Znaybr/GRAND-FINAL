3683d8d559ea47fede5bf226d0e3c9e10fa16ef8
ctack <christack50@gmail.com>
2016-02-26 14:12:49 -0500

    dossierapropos

---
+++ apropos.php
@@ -0,0 +1,13 @@
+<?php
+require_once '_defines.php';
+require_once 'common/start.php';
+require_once 'data/main_data.php';
+$site_data[PAGE_ID] = 'dashboard';
+require_once 'view_parts/_page_base.php';
+?>
+
+    <div id="main"></div>
+
+<?php
+require_once 'view_parts/_page_buttom.php';
+?>
