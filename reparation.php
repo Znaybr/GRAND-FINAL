e972758725676b5c5652f169340d11dcc30af4b6
ctack <christack50@gmail.com>
2016-02-26 14:27:50 -0500

    dossier reparation

---
+++ reparation.php
@@ -0,0 +1,15 @@
+<?php
+require_once '_defines.php';
+require_once 'common/start.php';
+require_once 'data/main_data.php';
+$site_data[PAGE_ID] = 'inscription';
+require_once 'view_parts/_page_base.php';
+
+?>
+
+<div id="main"></div>
+
+<?php
+require_once 'view_parts/_page_buttom.php';
+?>
+
