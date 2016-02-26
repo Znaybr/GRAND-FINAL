8c5a6bd5a035f9a19d40703fa4c7bf1016843684
ctack <christack50@gmail.com>
2016-02-26 14:26:43 -0500

    dossiercontact

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php
+require_once '_defines.php';
+require_once 'common/start.php';
+require_once 'data/main_data.php';
+$site_data[PAGE_ID] = 'contact';
+require_once 'view_parts/_page_base.php';
+require_once 'view_parts/_inscription_from.php';
+?>
+
+<div id="main"></div>
+
+<?php
+require_once 'view_parts/_page_buttom.php';
+?>
