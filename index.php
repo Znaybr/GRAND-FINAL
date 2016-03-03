4eb5c35a242320a30584f40e64b1d4e719a451fe
tverjus <thibaut.verjus@gmail.com>
2016-03-03 13:08:47 -0500

    Full Save 03/03/2016 - 13.08
    Reformatage css index

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ require_once 'view_parts/_page_base.php';
 ?>
 
 <div id="main">
+
+    <!--IMAGE A LA UNE-->
     <section id="img_princ">
         <?php
         $query = "SELECT * FROM produits  WHERE  id = 6";
@@ -19,183 +21,175 @@ require_once 'view_parts/_page_base.php';
         while ($row = $results->fetch(PDO::FETCH_ASSOC))
         {
             $row["id"];
-
             echo
-
-
                 "<div id= 'image_index' >" .
-
-                "<figure><img src='images/" .$row['illustration']."'></figure>".
-
+                    "<figure><img src='images/" .$row['illustration']."'></figure>".
                 "</div>".
-
                 "<div id='details'>".
-
-                "<p>Dernière réalisation</p>".
-
-                "<h3>".$row['nom'] ."</h3>".
-
-                "<figcaption>".$row['description'] ."</figcaption>".
-
+                    "<p>Dernière réalisation</p>".
+                    "<h3>".$row['nom'] ."</h3>".
+                    "<figcaption>".$row['description'] ."</figcaption>".
                 "</div>";
         }
-
         ?>
-    </section>
-    <img src="images/imagesweb/separation.png" alt="ligne de séparation" class="separ"/>
-    <section id="categorie">
 
+    </section>
+
+
+    <!--SOUS CATEGORIES-->
+    <section id="categorie">
+        <img src="images/imagesweb/separation.png" alt="ligne de séparation" class="separ"/>
+
+
+        <!--CATEGORIE 1-->
         <div id="cat1">
             <a href="?category_id=1">
-        <div id="cercle1" class="cercle">
-            <?php
-            $query = "SELECT * FROM produits  WHERE  categorie =1";
+                <div id="cercle1" class="cercle">
+                    <?php
+                    $query = "SELECT * FROM produits  WHERE  categorie =1";
 
-            $results = $pdo->prepare($query);
-            $results->execute();
-            while ($row = $results->fetch(PDO::FETCH_ASSOC))
-            {
-                $row["id"];
+                    $results = $pdo->prepare($query);
+                    $results->execute();
+                    while ($row = $results->fetch(PDO::FETCH_ASSOC))
+                    {
+                        $row["id"];
 
+                        echo
+                            "<style>"
+                            . "#cercle1{
+                            background-image:url("
+                            ." '" ."images". "/".$row['illustration']." ');
+                            background-size: cover;".
+                            "}"
+                            ."</style>";
+                    }
+                    ?>
 
-                echo
-
-                    "<style>"
-                    . "#cercle1{
-                    background-image:url("
-                    ." '" ."images". "/".$row['illustration']." ');
-                    background-size: cover;".
-                    "}"
-                    ."</style>";
-
-
-            }
-            ?>
-            <h3>Bagues</h3>
-        </div>
-
+                    <!--HOVER APPARITION AU SURVOL-->
+                    <div class="hover">
+                        <h3>Bagues</h3>
+                    </div>
+                </div>
             </a>
         </div>
 
+        <!--CATEGORIE 2-->
         <div id="cat2">
             <a href="?category_id=2">
-        <div id="cercle2" class="cercle">
-            <?php
-            $query = "SELECT * FROM produits  WHERE  categorie =2";
+                <div id="cercle2" class="cercle">
+                    <?php
+                    $query = "SELECT * FROM produits  WHERE  categorie =2";
 
-            $results = $pdo->prepare($query);
-            $results->execute();
-            //initialiser tableau vide
+                    $results = $pdo->prepare($query);
+                    $results->execute();
+                    //initialiser tableau vide
 
-            while ($row = $results->fetch(PDO::FETCH_ASSOC))
-            {
-                //ajouter valleur dans un tableau .push?
-                //array.push($row['illustration'])
-                //random avec comme limite array.lenght == nbrRandom
-                //`fin while
+                    while ($row = $results->fetch(PDO::FETCH_ASSOC))
+                    {
+                        //ajouter valleur dans un tableau .push?
+                        //array.push($row['illustration'])
+                        //random avec comme limite array.lenght == nbrRandom
+                        //`fin while
 
-                //echo
-//                "<style>"
-//                . "#cercle2{
-//                    background-image:url("
-//                ." '" ."images". "/".$array`[random]." ');
-//                    background-size: cover;".
-//                "}"
-////                ."</style>";
+                        //echo
+        //                "<style>"
+        //                . "#cercle2{
+        //                    background-image:url("
+        //                ." '" ."images". "/".$array`[random]." ');
+        //                    background-size: cover;".
+        //                "}"
+        ////                ."</style>";
+                        $row["id"];
 
+                        echo
+                            "<style>"
+                            . "#cercle2{
+                            background-image:url("
+                            ." '" ."images". "/".$row['illustration']." ');
+                            background-size: cover;".
+                            "}"
+                         ."</style>";
+                    }
 
-                $row["id"];
+                    ?>
 
-
-                echo
-
-                    "<style>"
-                    . "#cercle2{
-                    background-image:url("
-                    ." '" ."images". "/".$row['illustration']." ');
-                    background-size: cover;".
-                    "}"
-                 ."</style>";
-
-
-
-
-
-            }
-
-            ?>
-            <h3>Pendentifs</h3>
+                    <!--HOVER APPARITION AU SURVOL-->
+                    <div class="hover">
+                        <h3>Pendentifs</h3>
+                    </div>
+                </div>
+            </a>
         </div>
-                </a>
-        </div>
+
+        <!--CATEGORIE 3-->
         <div id="cat3">
             <a href="?category_id=3">
-        <div id="cercle3" class="cercle">
-            <?php
-            $query = "SELECT * FROM produits  WHERE  categorie =3";
+                <div id="cercle3" class="cercle">
+                    <?php
+                    $query = "SELECT * FROM produits  WHERE  categorie =3";
 
-            $results = $pdo->prepare($query);
-            $results->execute();
-            while ($row = $results->fetch(PDO::FETCH_ASSOC))
-            {
-                $row["id"];
+                    $results = $pdo->prepare($query);
+                    $results->execute();
+                    while ($row = $results->fetch(PDO::FETCH_ASSOC))
+                    {
+                        $row["id"];
 
+                        echo
+                            "<style>"
+                            . "#cercle3{
+                            background-image:url("
+                            ." '" ."images". "/".$row['illustration']." ');
+                            background-size: cover;".
+                            "}"
+                            ."</style>";
+                    }
+                    ?>
 
-                echo
-
-                    "<style>"
-                    . "#cercle3{
-                    background-image:url("
-                    ." '" ."images". "/".$row['illustration']." ');
-                    background-size: cover;".
-                    "}"
-                    ."</style>";
-
-
-
-
-
-            }
-            ?>
-            <h3>Boucles d'oreilles</h3>
+                    <!--HOVER APPARITION AU SURVOL-->
+                    <div class="hover">
+                        <h3>Boucles d'oreilles</h3>
+                    </div>
+                </div>
+            </a>
         </div>
-                </a>
-        </div>
+
+        <!--CATEGORIE 4-->
         <div id="cat4">
             <a href="?category_id=4">
-        <div id="cercle4" class="cercle">
-            <?php
-            $query = "SELECT * FROM produits  WHERE  categorie =4";
+                <div id="cercle4" class="cercle">
+                    <?php
+                    $query = "SELECT * FROM produits  WHERE  categorie =4";
 
-            $results = $pdo->prepare($query);
-            $results->execute();
-            while ($row = $results->fetch(PDO::FETCH_ASSOC))
-            {
-                $row["id"];
-
-
-                echo
-
-                    "<style>"
-                    . "#cercle4{
-                    background-image:url("
-                    ." '" ."images". "/".$row['illustration']." ');
-                    background-size: cover;".
-                    "}"
-                    ."</style>";
+                    $results = $pdo->prepare($query);
+                    $results->execute();
+                    while ($row = $results->fetch(PDO::FETCH_ASSOC))
+                    {
+                        $row["id"];
 
 
+                        echo
 
+                            "<style>"
+                            . "#cercle4{
+                            background-image:url("
+                            ." '" ."images". "/".$row['illustration']." ');
+                            background-size: cover;".
+                            "}"
+                            ."</style>";
 
+                    }
+                    ?>
 
-            }
-            ?>
-            <h3>Bracelets</h3>
+                    <!--HOVER APPARITION AU SURVOL-->
+                    <div class="hover">
+                        <h3>Bracelets</h3>
+                    </div>
+                </div>
+            </a>
         </div>
-                </a>
-        </div>
+
+        <img src="images/imagesweb/separation.png" alt="ligne de séparation" class="separ"/>
     </section>
-    <img src="images/imagesweb/separation.png" alt="ligne de séparation" class="separ"/>
 </div>
 
 <?php
